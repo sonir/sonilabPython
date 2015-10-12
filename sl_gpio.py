1ffca738b254adf2932c69b9965108e93094f81b
Sonir <isana@sonir.jp>
2015-10-12 18:11:45 +0900

    1st sl_gpio is stable

---
+++ sl_gpio.py
@@ -0,0 +1,48 @@
+#8CH GPIO TOGGLE
+import RPi.GPIO as GPIO
+
+PIN_A = 3
+PIN_B = 5
+PIN_C = 7
+PIN_D = 11
+PIN_E = 13
+PIN_F = 15
+PIN_G = 19
+PIN_H = 21
+CH_MAX = 8
+
+class SlGpio :
+
+    try:
+        #while True :
+        def __init__ (self) :
+            print("init")
+            self.CH_MAX = CH_MAX
+            self.GPIO = GPIO
+            self.GPIO.setmode(GPIO.BOARD)
+            global PIN_A, PIN_B, PIN_C, PIN_D, PIN_E, PIN_F, PIN_G, PIN_H
+            self.ch = [PIN_A, PIN_B, PIN_C, PIN_D, PIN_E, PIN_F, PIN_G, PIN_H]
+            self.ch_val = [False, False, False, False, False, False, False, False]
+
+            for var in range(CH_MAX):
+                print("The pin {0} was set as OUTPUT".format(self.ch[var]))
+                self.GPIO.setup(self.ch[var],GPIO.OUT)
+
+
+            # self.GPIO.setmode(GPIO.BOARD)
+                # self.GPIO.setup(self.pi,GPIO.OUT)
+
+        def set (self, ch, val) :
+            if ch >= CH_MAX or ch < 0:
+                print("The CH is invalid")
+                return False
+            else:
+                self.ch_val[ch]=val
+                return True
+
+        def update(self) :
+            for var in range(CH_MAX):
+                GPIO.output(self.ch[var],self.ch_val[var])
+
+    except KeyboardInterrupt :
+    	GPIO.cleanup()
---
+++ t_gpio.py
@@ -0,0 +1,29 @@
+import sl_gpio, time
+
+try:
+    gpio = sl_gpio.SlGpio()
+    gpio.set(7, True)
+    assert(gpio.ch_val[7]==True)
+    print("set val is OK")
+
+    assert(gpio.set(7, True)==True)
+    assert(gpio.set(8, True)==False)
+    assert(gpio.set(-1, True)==False)
+    print("CH Test is OK")
+
+    counter = 0
+    print("start: loop...")
+    while True :
+        for var in range(gpio.CH_MAX):
+            val = False
+            if var == counter:
+                val = True
+            gpio.set(var,val)
+        gpio.update()
+        counter = counter + 1
+        if counter >= gpio.CH_MAX:
+            counter = 0
+        time.sleep(0.2)
+
+except KeyboardInterrupt :
+	GPIO.cleanup()
