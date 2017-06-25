69c403b5acd4770d34c06fa5c1660ca2e94c7793
sonir <isana@sonir.jp>
2017-06-25 15:46:45 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from sonilab import sl_metro, sl_osc_send, osc_receive, event
+
+metro = sl_metro.Metro(1.0)
+sender = sl_osc_send.slOscSend("127.0.0.1" , 57137)
+receiver = osc_receive.OscReceive(57137)
+
+
+def osc_received (vals):
+    print "OSC RECEIVED :: arg[0] = " + str(vals[0]) + " | arg[1] = "  + str(vals[1])
+
+event.add("/test" , osc_received)
+receiver.setup("/foo")
+
+
+try :
+    while True:
+        if metro.update():
+            print "OK"
+            sender.send("/test", 137, 1.37)
+
+except KeyboardInterrupt :
+    receiver.terminate()
