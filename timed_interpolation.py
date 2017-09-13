547778efa6bf607a5987a35a8c75d829e115cb01
sonir <isana@sonir.jp>
2017-09-14 01:33:50 +0900

    5.22C :: mod timed_interpolation current bug

--- timed_interpolation.py
+++ timed_interpolation.py
@@ -11,7 +11,6 @@ class TimedInterpolation :
         self.duration = 0.0
         self.time_st = 0.0
         self.time_ed = 0.0
-        self.time_diff = 0.0
         self.time_now = 0.0
         self.ellapse = 0.0
 
@@ -19,6 +18,7 @@ class TimedInterpolation :
 
 
     def manual_set(self, st, ed, sec):
+
         self.st = st
         self.ed = ed
         self.duration = sec
@@ -35,12 +35,42 @@ class TimedInterpolation :
     def update(self):
 
         if self.end_flg == True or self.duration == 0.0:
-            return self.ed
-
-        self.time_now = time.time()
-        self.ellapse = self.time_now - self.time_st
-        self.current = self.st + ( self.diff * ( self.ellapse / self.duration ) )
-        if self.ellapse > self.duration:
             self.current = self.ed
-            self.end_flg = True
-        return self.current
+            return self.current
+        else:
+            self.time_now = time.time()
+            self.ellapse = self.time_now - self.time_st
+            self.current = self.st + ( self.diff * ( self.ellapse / self.duration ) )
+            if self.ellapse > self.duration:
+                self.current = self.ed
+                self.end_flg = True
+            return self.current
+
+
+    def update_exponential(self):
+
+        if self.end_flg == True or self.duration == 0.0:
+            self.current = self.ed
+            return self.current
+        else:
+            self.time_now = time.time()
+            self.ellapse = self.time_now - self.time_st
+            self.current = self.st + ( self.diff * ( (self.ellapse / self.duration)*(self.ellapse / self.duration) ) )
+            if self.ellapse > self.duration:
+                self.current = self.ed
+                self.end_flg = True
+            return self.current
+
+
+    def print_params(self):
+        print "duration: " , self.duration
+        print "time_st: " , self.time_st
+        print "time_ed: " , self.time_ed
+        print "time_now" , self.time_now
+        print "ellapse: " , self.ellapse
+        print "st: " , self.st
+        print "ed: " , self.ed
+        print "diff: " , self.diff
+        print "current: " , self.current
+
+
--- ut_timed_interpolation.py
+++ ut_timed_interpolation.py
@@ -4,15 +4,40 @@ import timed_interpolation, sl_metro
 metro = sl_metro.Metro(1.0)
 
 lerp = timed_interpolation.TimedInterpolation()
-lerp.manual_set(10.0, 0.0, 10.0)
+lerp.manual_set(10.0, 0.1, 10.0)
 count = 0
-while True:
+
+tmp = lerp.update()
+
+print "check manual set with starting value , end value and the duration"
+while tmp != 0.1:
     if metro.update():
         tmp = lerp.update()
         print tmp
-        if tmp == 0.0:
-            metro.set(1.0)
-            lerp.set(10.0, 10.0)
-        if tmp == 10:
-            print "OK"
-            exit()
+
+assert lerp.update() == 0.1
+print "Finished."
+
+
+print "check automatic set without st time"
+lerp.set(10.0, 10.0)
+while tmp != 10.0:
+    if metro.update():
+        tmp = lerp.update()
+        print tmp
+print "--"
+
+print "Check direct set with duration 0.0"
+lerp.manual_set(13.7, 137.0, 0.0)
+assert lerp.update() == 137.0
+
+# print "Check automatic set with duration 0.0"
+lerp.set(13.7, 0.0)
+assert lerp.update() == 13.7
+
+
+
+print "ALL OK"
+exit()
+
+
