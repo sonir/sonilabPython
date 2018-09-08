690230169456331fb9900c04d57ab16a9cb4b79a
sonir <isana@sonir.jp>
2018-09-09 02:06:16 +0900

    5.5 :: fixed event_cue using while(must)

--- event_cue.py
+++ event_cue.py
@@ -8,11 +8,9 @@ class EventCue:
 		self.deq = deque()
 
 	def add(self,adr, *args):
-		self.deq.append((adr, args))
+		self.deq.append([adr, args])
 
 	def dump(self):
-		for elm in self.deq:
+		while len(self.deq):
+			elm = self.deq.popleft()
 			event.bang(elm[0],*elm[1])
-			# print elm[0]
-			# for elm2 in elm[1]:
-			# 	print "arg:", elm2
